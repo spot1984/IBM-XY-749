83e2d5a037076e09eb825f40f1cf3a40d01193d5
D. Scott Williamson <spot1984@gmail.com>
2024-06-10 20:01:44 -0500

    Added comments and luminance weighted based color comparison (need Lab CIE)

--- color.py
+++ color.py
@@ -1,14 +1,26 @@
 from random import randint
 
+# create a color from a string
+def colorFromString(s):
+    i=0
+    if s[0:1]=='#':
+        i=1
+    r=int(s[i:i+2],16)
+    g=int(s[i+2:i+4],16)
+    b=int(s[i+4:i+6],16)
+    return Color(r,g,b)
+
 class Color:
     r=0
     g=0
     b=0
+    # initialize from values
     def __init__(self,r,g,b):
         self.r=r    
         self.g=g
         self.b=b
-    
+      
+    # increment color
     def inc(self,d):
         self.r+=d
         if self.r>255:
@@ -19,16 +31,22 @@ class Color:
             self.b=self.b+d
         if self.b>255:
             self.b=0    
-    
-    def tuple(self):
-        return(self.r,self.g,self.b)
-    
+
+    # compare this color to another
     def difference(self,other):
         # constants for relative importance from luminance calculation
         # integers used 
+        #return  abs(self.r-other.r)+ \
+        #        abs(self.g-other.g)+ \
+        #        abs(self.b-other.b)
         return  299*abs(self.r-other.r)+ \
                 587*abs(self.g-other.g)+ \
                 114*abs(self.b-other.b)
+        
+    # return as a tuple
+    def tuple(self):
+        return(self.r,self.g,self.b)
     
+    # return a random color
     def rnd(self):
         return(randint(0,256),randint(0,256),randint(0,256))
